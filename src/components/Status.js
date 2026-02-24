406d492e189c75d5d170e2507fd4a3e988f15cd6
SHAMIM <111671483+Shamim-97@users.noreply.github.com>
2026-02-24 12:21:02 +0600

    fix: Texty connect status issue

--- src/components/Status.js
+++ src/components/Status.js
@@ -2,37 +2,50 @@ import { useState, useEffect } from 'react';
 import { __ } from '@wordpress/i18n';
 import apiFetch from '@wordpress/api-fetch';
 import { Notice, Icon, Spinner } from '@wordpress/components';
+import domReady from '@wordpress/dom-ready';
 
 function Status() {
   const [isConnected, setIsConnected] = useState(false);
   const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
-    setIsFetching(true);
+      setIsFetching(true);
+      try {
+        apiFetch({
+          path: '/texty/v1/status',
+        }).then((resp) => {
+          console.log('Fetching status.. complete', resp);
+          setIsConnected(resp.success);
+          setIsFetching(false);
 
-    apiFetch({
-      path: '/texty/v1/status',
-    }).then((resp) => {
-      setIsFetching(false);
-      setIsConnected(resp.success);
-    });
-  }, []);
 
-  if (isFetching) {
-    return (
+        });
+      } catch(error) {
+        console.error('Error fetching status:', error);
+      } finally {
+      }
+  }, [isConnected]);
+
+  const FetchStatus = () =>  (
       <Notice>
         <Spinner />
       </Notice>
     );
-  }
+  
 
-  return (
+  const StatusNotice = () => (
     <Notice status={isConnected ? 'success' : 'error'} isDismissible={false}>
       <Icon icon={isConnected ? 'yes-alt' : 'dismiss'} />
       <span>
         {isConnected ? __('Connected', 'texty') : __('Not connected', 'texty')}
       </span>
     </Notice>
+  );
+
+  return (
+    <div className="texty-status">
+      {isFetching ? <FetchStatus /> : <StatusNotice />}
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import { createRoot } from '@wordpress/element';
+import domReady from '@wordpress/dom-ready';
+
 import menuFix from './utils/admin-menu-fix';
 import App from './App';
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-phone-input-2/lib/style.css';
 import './style.scss';
 
-const mountNode = document.getElementById('texty-app');
-const root = createRoot(mountNode);
-root.render(<App />);
+domReady(() => {
 
-menuFix('texty');
+    const mountNode = document.getElementById('texty-app');
+    const root = createRoot(mountNode);
+    root.render(<App />);
+
+    menuFix('texty');
+});
