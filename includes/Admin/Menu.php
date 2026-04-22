4f6507c89fce0dcb538b4c2adcf93a590a26b0d2
Kamruzzaman <kzamanbn@gmail.com>
2026-04-22 23:49:31 +0600

    feat(menu): enhance admin menu registration with capability checks and dynamic submenus

--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -8,31 +8,72 @@ namespace Texty\Admin;
 class Menu {
 
     /**
-     * Initialize
+     * Class constructor
+     *
+     * @return void
      */
     public function __construct() {
         add_action( 'admin_menu', [ $this, 'register_menu' ] );
     }
 
     /**
-     * Register admin menu
+     * Add Texty admin menu
      *
      * @return void
      */
     public function register_menu() {
-        $position = apply_filters( 'texty_menu_position', 58 );
+        global $submenu;
 
-        $menu = add_menu_page(
+        $capability = apply_filters( 'texty_admin_menu_capability', 'manage_options' );
+        if ( ! current_user_can( $capability ) ) {
+            return;
+        }
+
+        $menu_position = apply_filters( 'texty_menu_position', 58 );
+        $slug          = 'texty';
+        $menu_icon     = 'data:image/svg+xml;base64,' . base64_encode( '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#a0a5aa" d="M10.52 1c.603 0 1.191.056 1.761.164a5.358 5.358 0 00-1.1 2.754 6.538 6.538 0 00-5.737 10.626l.629.773-.966 1.645h5.414a6.538 6.538 0 006.511-7.138 5.355 5.355 0 002.764-1.075 9.423 9.423 0 01-9.275 11.097H0l2.602-4.314-.013-.02A9.423 9.423 0 0110.521 1zm6.018 0a3.462 3.462 0 110 6.923 3.462 3.462 0 010-6.923z"/></svg>' ); // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
+
+        $dashboard = add_menu_page(
             __( 'Texty', 'texty' ),
             __( 'Texty', 'texty' ),
-            'manage_options',
-            'texty',
+            $capability,
+            $slug,
             [ $this, 'render_page' ],
-            'data:image/svg+xml;base64,' . base64_encode( '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#a0a5aa" d="M10.52 1c.603 0 1.191.056 1.761.164a5.358 5.358 0 00-1.1 2.754 6.538 6.538 0 00-5.737 10.626l.629.773-.966 1.645h5.414a6.538 6.538 0 006.511-7.138 5.355 5.355 0 002.764-1.075 9.423 9.423 0 01-9.275 11.097H0l2.602-4.314-.013-.02A9.423 9.423 0 0110.521 1zm6.018 0a3.462 3.462 0 110 6.923 3.462 3.462 0 010-6.923z"/></svg>' ), // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
-            $position
+            $menu_icon,
+            $menu_position
         );
 
-        add_action( 'admin_print_scripts-' . $menu, [ $this, 'enqueue_scripts' ] );
+        $submenus = apply_filters(
+            'texty_admin_menu',
+            [
+                [
+					'id' => 'dashboard',
+					'title' => __( 'Dashboard', 'texty' ),
+				],
+                [
+					'id' => 'notifications',
+					'title' => __( 'Notifications', 'texty' ),
+				],
+                [
+					'id' => 'tools',
+					'title' => __( 'Tools', 'texty' ),
+				],
+            ],
+            $capability,
+            $slug
+        );
+
+        foreach ( $submenus as $item ) {
+            $submenu[ $slug ][] = [ // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+                $item['title'],
+                $capability,
+                'admin.php?page=' . $slug . '#/' . $item['id'],
+            ];
+        }
+
+        do_action( 'texty_admin_menu', $capability, $menu_position );
+
+        add_action( 'admin_print_scripts-' . $dashboard, [ $this, 'enqueue_scripts' ] );
     }
 
     /**
