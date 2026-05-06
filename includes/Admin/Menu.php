19fd1012250b32b7ae04fc93ec5b744e2e68820e
Kamruzzaman <kzamanbn@gmail.com>
2026-05-06 17:31:54 +0600

    feat: update API class registration and enhance version handling in Header component

--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -143,6 +143,9 @@ class Menu {
             'rest_url'  => esc_url_raw( rest_url() ),
             'ajax_url'  => esc_url_raw( admin_url( 'admin-ajax.php' ) ),
             'nonce'     => wp_create_nonce( 'wp_rest' ),
+            'version'   => [
+                'lite' => TEXTY_VERSION,
+            ],
         ];
 
         return apply_filters( 'texty_localize_script', $i18n );
